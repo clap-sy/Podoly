734bc87223e8d3eb652a0211ee126cb9b5ca1011
PYS <tndusdl0228123@naver.com>
2023-11-12 22:15:31 +0900

    first commit

---
+++ src/main/java/kr/ac/podoly/PodolyApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.podoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodolyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/podoly/PodolyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.podoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
